b56d9da00594c8b99eea0e01badf28a3207032b1
Alexander <veber21@rambler.ru>
2024-07-11 10:19:07 +0300

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -1,8 +1,8 @@
-def get_multiplied_digits(number):  # рекурсивная функция для подсчета произведения цифр кроме 0
+def get_multiplied_digits(number):  # рекурсивная функция для подсчета произведения цифр
     str_number = str(number)
     first = int(str_number[0])
     if len(str_number) == 1:
-        return int(str_number[0])
+        return first
     else:
         return first * get_multiplied_digits(int(str_number[1:]))
 
